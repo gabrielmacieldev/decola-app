24a2e8727afd066c5f884e63995109b4ccf8e9e5
Wendel Gomes <wendelgomesdh7@gmail.com>
2023-08-19 16:14:32 -0400

    atualizada a branch principal

--- src/router/Router.js
+++ src/router/Router.js
@@ -5,11 +5,7 @@ import { Form } from "../Pages/Form/Form"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ContactDetails from "../Pages/Contact Details/ContactDetails";
 import Training from "../Pages/Training/Training";
-
 import Experience from "../Pages/Experience/Experience";
-
-import Experience from "../Pages/Experience/Experience";
-
 
 const Router = () => {
 
@@ -21,11 +17,7 @@ const Router = () => {
                 <Route path="/form" element={<Form/>} />
                 <Route path="/contactdetails" element={<ContactDetails/>} />
                 <Route path="/Training" element={<Training/>} />
-
                 <Route path="/Experience" element={<Experience/>} />
-
-                <Route path="/Experience" element={<Experience/>} />
-
             </Routes>
         </BrowserRouter>
     )
