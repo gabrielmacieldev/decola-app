7bc2c161500822287f246292a192e3bb55e5516c
Wendel Gomes <wendelgomesdh7@gmail.com>
2023-08-24 09:17:28 -0400

    finalizado a page About

---
+++ src/Pages/About/About.js
@@ -0,0 +1,101 @@
+import React, { useState } from 'react'
+import { Container } from './AboutStyle'
+
+
+const About = () => {
+
+    const [etnia, setEtnia] = useState("")
+    const [genero, setGenero] = useState("")
+    const [sexual, setSexual] = useState("")
+    const [deficiencia, setDeficiencia] = useState("")
+    const [mora, setMora] = useState("")
+    const [medio, setMedio] = useState("")
+    const [renda, setRenda] = useState("")
+    const [bolsa, setBolsa] = useState("")
+
+    return (
+
+        <Container>
+
+            <div className='sobre'>
+                <div className='h1'>
+                </div>
+                <h1>Sobre você</h1>
+                <p>O nosso objetivo é saber mais sobre quais perfis estamos conseguindo atingir para melhorarmos cada vez mais as experiências para os desenvolvedores do Brasil na busca pelo trabalho.</p>
+            </div>
+
+            <div className='sobre'>
+                <div className='h1'>
+                </div>
+                <strong>Racialmente, você se considera uma pessoa</strong>
+                <p>As respostas abaixo são baseadas na classificação do IBGE</p>
+
+                <input
+                    value={etnia}
+                    onChange={(e) => setEtnia(e.target.value)}
+                    placeholder='   Selecione sua etnia'
+                    ></input>
+
+                <strong>Qual o seu gênero?</strong>
+                <input
+                    value={genero}
+                    onChange={(e) => setGenero(e.target.value)}
+                    placeholder='   Selecione sua gênero'
+                    ></input>
+
+                <strong>Qual a sua orientação sexual?</strong>
+                <input
+                    value={sexual}
+                    onChange={(e) => setSexual(e.target.value)}
+                    placeholder='   Selecione sua orientação sexual'
+                    ></input>
+
+                <strong>Você possui alguma deficiência?</strong>
+                <p>A deficiência pode ser física, mental, auditiva, visual ou múltipla. A sigla utilizada é PCD</p>
+
+                <input
+                    value={deficiencia}
+                    onChange={(e) => setDeficiencia(e.target.value)}
+                    placeholder='   Você tem algum tipo de deficiência?'
+                    ></input>
+
+                <strong>Como é o lugar onde você mora?</strong>
+                <input
+                    value={mora}
+                    onChange={(e) => setMora(e.target.value)}
+                    placeholder='   Como é o lugar onde você mora?'
+                    ></input>
+
+                <strong>Você cursou o ensino médio em:</strong>
+                <input
+                    value={medio}
+                    onChange={(e) => setMedio(e.target.value)}
+                    placeholder='   Selecione o tipo de Instituição de Ensino.'
+                    ></input>
+
+                <strong>Qual das seguintes alternativas melhor expressa a situação atual da sua renda familiar?</strong>
+                <input
+                    value={renda}
+                    onChange={(e) => setRenda(e.target.value)}
+                    placeholder='   Selecione uma opção'
+                    ></input>
+
+                <strong>Você possui bolsa de estudos?</strong>
+                <input
+                    value={bolsa}
+                    onChange={(e) => setBolsa(e.target.value)}
+                    placeholder='   Selecione uma opção'
+                    ></input>
+
+                <button>Salvar informações</button>
+            </div>
+
+            <div className='btn'>
+                <button className='voltar'>Voltar</button>
+                <button className='proximo'>Tudo certo!</button>
+            </div>
+        </Container>
+    )
+}
+
+export default About
---
+++ src/Pages/About/AboutStyle.js
@@ -0,0 +1,391 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+background: lightgrey;
+position: relative;
+
+.sobre {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background: rgba(255, 255, 255, .30);
+    border: 2px solid #ffffff30;
+    color: #3c2166;
+    backdrop-filter: blur(15px);
+    width: 30rem;
+    min-height: 10rem;
+    margin-top: 2rem;
+    border-radius: 25px;
+    padding: 30px;
+
+    h1 {
+        margin-bottom: 30px;
+        font-size: 30px;
+        text-align: start;
+    }
+
+    input {
+        width: 340px;
+        margin: 8px;
+        padding: 12px;
+        
+    }
+    button {
+            margin-right: 214px;
+            background: #f3f3f3;
+            border-radius: 15px;
+            border: 2px solid white;
+            width: 36%;
+            height: 45px;
+            font-size: 16px;
+            font-weight: 900;
+            color: #3c2166;
+            cursor: pointer;
+            margin-top: 12px;
+            margin-bottom: 30px;
+            margin-left: 18px;
+    }
+    .btn-formacao {
+            margin-left: 209px;
+            background: #3c2166;
+            border-radius: 15px;
+            border: 2px solid white;
+            width: 83%;
+            height: 55px;
+            font-size: 20px;
+            font-weight: 900;
+            color: #f3f3f3;
+            cursor: pointer;
+            margin-top: 8px;
+            margin-bottom: 8px;
+    } 
+
+    @media (max-width: 550px) {
+        width: 26rem;
+        h1 { 
+           font-size: 28px;
+           text-align: center;
+        }
+
+        p {
+            font-size: 21px;
+        }
+}
+    @media (max-width: 470px) {
+        width: 20rem;
+       h1 { 
+           font-size: 25px;
+           text-align: center;
+        }
+    input {
+        width: 250px;
+    }
+    button {
+        width: 50%;
+        font-size: 15px;
+        margin-left: 6rem;
+    }
+    .btn-formacao {
+        font-size: 15px;
+        width: 80%;
+    }
+}
+    @media (max-width: 402px) {
+        width: 18rem;
+        h1{
+            font-size: 22px;  
+        }
+        p{
+            font-size: 18px;
+            margin-top: 1rem;
+        }
+        button {
+            width: 50%;
+            font-size: 15px;
+            margin-left: 6rem;
+        }
+        img {
+        width: 200px;
+    } 
+}
+}
+
+.idioma {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background: rgba(255, 255, 255, .30);
+    border: 2px solid #ffffff30;
+    color: #3c2166;
+    backdrop-filter: blur(15px);
+    width: 30rem;
+    min-height: 10rem;
+    margin-top: 2rem;
+    border-radius: 25px;
+    padding: 30px;
+
+    h1 {
+        margin-bottom: 30px;
+        font-size: 30px;
+        text-align: start;
+    }
+
+    input {
+        width: 340px;
+        margin: 8px;
+        padding: 12px;
+        
+    }
+    button {
+            margin-right: 214px;
+            background: #f3f3f3;
+            border-radius: 15px;
+            border: 2px solid white;
+            width: 36%;
+            height: 45px;
+            font-size: 16px;
+            font-weight: 900;
+            color: #3c2166;
+            cursor: pointer;
+            margin-top: 12px;
+            margin-bottom: 30px;
+            margin-left: 18px;
+    }
+    .btn-idioma {
+            margin-left: 209px;
+            background: #3c2166;
+            border-radius: 15px;
+            border: 2px solid white;
+            width: 83%;
+            height: 55px;
+            font-size: 20px;
+            font-weight: 900;
+            color: #f3f3f3;
+            cursor: pointer;
+            margin-top: 8px;
+            margin-bottom: 8px;
+    } 
+
+    @media (max-width: 550px) {
+        width: 26rem;
+        h1 { 
+           font-size: 28px;
+           text-align: center;
+        }
+        p {
+            font-size: 21px;
+        }
+}
+    @media (max-width: 470px) {
+        width: 20rem;
+    h1 { 
+           font-size: 25px;
+           text-align: center;
+        }
+    input {
+        width: 250px;
+    }
+    button {
+        width: 50%;
+        font-size: 15px;
+        margin-left: 6rem;
+    }
+    .btn-idioma {
+        font-size: 15px;
+        width: 80%;
+    }
+}
+    @media (max-width: 402px) {
+        width: 18rem;
+        h1{
+            font-size: 22px;  
+        }
+        p{
+            font-size: 18px;
+            margin-top: 1rem;
+        }
+        button {
+            width: 50%;
+            font-size: 15px;
+            margin-left: 6rem;
+        }
+        img {
+        width: 200px;
+    } 
+}
+}
+.ferramentas {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background: rgba(255, 255, 255, .30);
+    border: 2px solid #ffffff30;
+    color: #3c2166;
+    backdrop-filter: blur(15px);
+    width: 30rem;
+    min-height: 10rem;
+    margin-top: 2rem;
+    border-radius: 25px;
+    padding: 30px;
+    margin-bottom: 30px;
+
+    h1 {
+        margin-bottom: 30px;
+        font-size: 30px;
+        text-align: start;
+    }
+
+    input {
+        width: 340px;
+        margin: 8px;
+        padding: 12px;  
+    }
+
+    button {
+            margin-right: 214px;
+            background: #f3f3f3;
+            border-radius: 15px;
+            border: 2px solid white;
+            width: 36%;
+            height: 45px;
+            font-size: 16px;
+            font-weight: 900;
+            color: #3c2166;
+            cursor: pointer;
+            margin-top: 12px;
+            margin-bottom: 30px;
+            margin-left: 18px;
+    }
+    .btn-ferramentas {
+            margin-left: 209px;
+            background: #3c2166;
+            border-radius: 15px;
+            border: 2px solid white;
+            width: 83%;
+            height: 55px;
+            font-size: 20px;
+            font-weight: 900;
+            color: #f3f3f3;
+            cursor: pointer;
+            margin-top: 8px;
+            margin-bottom: 8px;
+    } 
+
+    @media (max-width: 550px) {
+        width: 26rem;
+        h1 { 
+           font-size: 28px;
+           text-align: center;
+        }
+        p {
+            font-size: 21px;
+        }
+}
+    @media (max-width: 470px) {
+        width: 20rem;
+       h1 { 
+           font-size: 25px;
+           text-align: center;
+        }
+    input {
+        width: 250px;
+    }
+    button {
+        width: 50%;
+        font-size: 15px;
+        margin-left: 6rem;
+    }
+    .btn-ferramentas {
+        font-size: 15px;
+        width: 80%;
+    }
+}
+    @media (max-width: 402px) {
+        width: 18rem;
+        h1{
+            font-size: 22px;  
+        }
+        p{
+            font-size: 18px;
+            margin-top: 1rem;
+        }
+        button {
+            width: 50%;
+            font-size: 15px;
+            margin-left: 6rem;
+        }
+        .btn-ferramentas {
+        font-size: 15px;
+        width: 90%;
+    }
+}
+}
+
+.btn {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 50px;
+    margin-top: 12px;
+.voltar {
+    margin: 0 8px;
+    padding: 1px 70px;
+    height: 55px;
+    font-size: 20px;
+    border-radius: 15px;
+    border: 2px solid transparent;
+    background-color: #f3f3f3;
+    color: #3c2166;
+    font-weight: 900;
+    cursor: pointer;
+}
+.proximo {
+    margin: 0 8px;
+    padding: 1px 70px;
+    height: 55px;
+    font-size: 20px;
+    border-radius: 15px;
+    border: 2px solid transparent;
+    background-color: #3c2166;
+    color: #f3f3f3;
+    font-weight: 900;
+    cursor: pointer;
+}
+.proximo:hover {
+    background-color: #f3f3f3;
+    color: #3c2166;
+}
+@media (max-width: 550px) {
+        .proximo {
+            padding: 1px 55px;  
+        }
+        .voltar {
+            padding: 1px 55px;
+        }     
+}
+    @media (max-width: 470px) {
+        .proximo {
+            padding: 1px 35px;  
+        }
+        .voltar {
+            padding: 1px 35px;
+        }   
+}
+    @media (max-width: 402px) {
+
+        .proximo {
+            padding: 1px 25px;  
+        }
+        .voltar {
+            padding: 1px 25px;
+        }
+}
+}
+`
